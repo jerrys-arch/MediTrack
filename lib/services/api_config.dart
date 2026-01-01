b8f0a7b9759497c76a7e5fb135da1a85360ebe7b
Eyerus Molla <eyerusmolla29@gmail.com>
2026-01-01 05:10:36 -0800

    api config update

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,23 +1,12 @@
-import 'package:flutter/foundation.dart';
 
 class ApiConfig {
-  // Your PC's local IP (real phone access)
-  static const String _androidPhysical =
-      "http://192.168.1.102:8000/api";
-
-  // Android Emulator
- // static const String _androidEmulator =
-   //   "http://10.0.2.2:8000/api";
-
-  // Web / Desktop
-  static const String _localhost =
-      "http://localhost:8000/api";
+  // Live backend URL
+  static const String _live = "https://meditrack-7.onrender.com/api";
 
   static String get baseUrl {
-    if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-      return _androidPhysical; // REAL PHONE
-    }
-    return _localhost;
+    // You can still differentiate between Web and Android if needed
+    // But for deployment, we use live URL everywhere
+    return _live;
   }
 
   static String get login => "$baseUrl/auth/login/";
